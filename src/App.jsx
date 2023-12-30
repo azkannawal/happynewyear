a492f34da677b4419490ae43e29ca26d9e8d821e
azkannawal <azkannawalaf@gmail.com>
2023-12-31 00:21:45 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import YouTube from "react-youtube";
+import styled from "styled-components";
+
+const App = () => {
+  const playlistId = "PLclODpQJhNz7vm19Ab3k31tUX7dncoup4";
+  const [player, setPlayer] = useState(null);
+
+  const opts = {
+    height: "0",
+    width: "0",
+    playerVars: {
+      listType: "playlist",
+      list: playlistId,
+      autoplay: 0, // Set autoplay to 0 to prevent immediate autoplay
+      mute: 0,
+      playsinline: 1,
+      origin: window.location.origin,
+    },
+  };
+
+  const playVideo = () => {
+    if (player) {
+      player.playVideo();
+    }
+  };
+
+  const onReady = (event) => {
+    setPlayer(event.target);
+  };
+
+  return (
+    <Container>
+        <Button onClick={playVideo}>Play Video</Button>
+        <YouTube opts={opts} onReady={onReady} />
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+const Button = styled.div`
+  z-index: 1;
+  cursor: pointer;
+  padding: 10px 12px;
+  background-color: maroon;
+  border-radius: 1em;
+`;
+export default App;
